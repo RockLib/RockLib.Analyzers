6cf8787f4f7b7f4a45b4506d5fc02133f25c8ded
Jason Bock <jasonbock1@rocketmortgage.com>
2022-02-16 08:27:10 -0600

    #139 273 pass, 60 fail (most are failing under .NET 4.8 FWIW)

--- Logging/RockLib.Logging.Analyzers.Test/NoLogLevelSpecifiedCodeFixProviderTests.cs
+++ Logging/RockLib.Logging.Analyzers.Test/NoLogLevelSpecifiedCodeFixProviderTests.cs
@@ -6,10 +6,42 @@ namespace RockLib.Logging.Analyzers.Test
     public class NoLogLevelSpecifiedCodeFixProviderTests
     {
         [Fact(DisplayName = null)]
-        public async Task CodeFixApplied1()
+        public async Task VerifyWhenLevelIsNotSpecifiedViaParameter()
         {
-            await TestAssistants.VerifyCodeFixAsync<NoLogLevelSpecifiedAnalyzer, NoLogLevelSpecifiedCodeFixProvider>(@"
-using RockLib.Logging;
+            await TestAssistants.VerifyCodeFixAsync<NoLogLevelSpecifiedAnalyzer, NoLogLevelSpecifiedCodeFixProvider>(
+@"using RockLib.Logging;
+using System;
+
+public class Test
+{
+    public void Call_Log_With_LogEntry_With_Level_Not_Set(ILogger logger)
+    {
+        logger.Log([|new LogEntry(""Hello, world!"")|]);
+
+        LogEntry logEntry2 = new(""Hello, world!"");
+        logger.Log([|logEntry2|]);
+    }
+}", 
+@"using RockLib.Logging;
+using System;
+
+public class Test
+{
+    public void Call_Log_With_LogEntry_With_Level_Not_Set(ILogger logger)
+    {
+        logger.Log(new LogEntry(""Hello, world!"", level: LogLevel.Debug));
+
+        LogEntry logEntry2 = new(""Hello, world!"", level: LogLevel.Debug);
+        logger.Log(logEntry2);
+    }
+}").ConfigureAwait(false);
+        }
+        
+        [Fact(DisplayName = null)]
+        public async Task VerifyWhenLevelIsNotSpecifiedViaProperty()
+        {
+            await TestAssistants.VerifyCodeFixAsync<NoLogLevelSpecifiedAnalyzer, NoLogLevelSpecifiedCodeFixProvider>(
+@"using RockLib.Logging;
 using System;
 
 public class Test
@@ -20,16 +52,11 @@ public class Test
 
         logger.Log([|new LogEntry { Message = ""Hello, world!"" }|]);
 
-        logger.Log([|new LogEntry(""Hello, world!"")|]);
-
         LogEntry logEntry1 = new();
         logger.Log([|logEntry1|]);
-
-        LogEntry logEntry2 = new(""Hello, world!"");
-        logger.Log([|logEntry2|]);
     }
-}", @"
-using RockLib.Logging;
+}", 
+@"using RockLib.Logging;
 using System;
 
 public class Test
@@ -40,16 +67,11 @@ public class Test
 
         logger.Log(new LogEntry { Message = ""Hello, world!"", Level = LogLevel.Debug });
 
-        logger.Log(new LogEntry(""Hello, world!"", level: LogLevel.Debug));
-
         LogEntry logEntry1 = new()
         {
             Level = LogLevel.Debug
         };
         logger.Log(logEntry1);
-
-        LogEntry logEntry2 = new(""Hello, world!"", level: LogLevel.Debug);
-        logger.Log(logEntry2);
     }
 }").ConfigureAwait(false);
         }
