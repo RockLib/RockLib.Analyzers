e504e881b658f7e34ef264ea613bb8d42d0cd9ac
Jason Bock <jasonbock1@rocketmortgage.com>
2022-02-17 12:48:55 -0600

    #140 Projects are updated, changes made, code builds, and tests pass!

--- Logging.AspNetCore/RockLib.Logging.AspNetCore.Analyzers/AddInfoLogAttributeAnalyzer.cs
+++ Logging.AspNetCore/RockLib.Logging.AspNetCore.Analyzers/AddInfoLogAttributeAnalyzer.cs
@@ -1,17 +1,19 @@
-﻿﻿using Microsoft.CodeAnalysis;
+﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Operations;
 using RockLib.Analyzers.Common;
+using System;
 using System.Collections.Immutable;
+using System.Globalization;
 using System.Linq;
 using System.Threading;
 
 namespace RockLib.Logging.AspNetCore.Analyzers
 {
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    public class AddInfoLogAttributeAnalyzer : DiagnosticAnalyzer
+    public sealed class AddInfoLogAttributeAnalyzer : DiagnosticAnalyzer
     {
         private static readonly LocalizableString _title = "Add InfoLog attribute";
         private static readonly LocalizableString _messageFormat = "Add an [InfoLog] attribute to the {0}";
@@ -25,12 +27,13 @@ namespace RockLib.Logging.AspNetCore.Analyzers
             DiagnosticSeverity.Info,
             isEnabledByDefault: true,
             description: _description,
-            helpLinkUri: string.Format(HelpLinkUri.Format, DiagnosticIds.UseSanitizingLoggingMethod));
+            helpLinkUri: string.Format(CultureInfo.InvariantCulture, HelpLinkUri.Format, DiagnosticIds.UseSanitizingLoggingMethod));
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
 
         public override void Initialize(AnalysisContext context)
         {
+            if (context is null) { throw new ArgumentNullException(nameof(context)); }
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
             context.EnableConcurrentExecution();
             context.RegisterCompilationStartAction(OnCompilationStart);
@@ -39,24 +42,34 @@ namespace RockLib.Logging.AspNetCore.Analyzers
         private static void OnCompilationStart(CompilationStartAnalysisContext context)
         {
             var infoLogAttributeType = context.Compilation.GetTypeByMetadataName("RockLib.Logging.AspNetCore.InfoLogAttribute");
-            if (infoLogAttributeType == null)
-                return;
+            if (infoLogAttributeType == null) 
+            { 
+                return; 
+            }
 
             var nonControllerAttributeType = context.Compilation.GetTypeByMetadataName("Microsoft.AspNetCore.Mvc.NonControllerAttribute");
             if (nonControllerAttributeType == null)
+            {
                 return;
+            }
 
             var controllerAttributeType = context.Compilation.GetTypeByMetadataName("Microsoft.AspNetCore.Mvc.ControllerAttribute");
             if (controllerAttributeType == null)
+            {
                 return;
+            }
 
             var nonActionAttributeType = context.Compilation.GetTypeByMetadataName("Microsoft.AspNetCore.Mvc.NonActionAttribute");
             if (nonActionAttributeType == null)
+            {
                 return;
+            }
 
             var filterCollectionType = context.Compilation.GetTypeByMetadataName("Microsoft.AspNetCore.Mvc.Filters.FilterCollection");
             if (filterCollectionType == null)
+            {
                 return;
+            }
 
             var analyzer = new Analyzer(infoLogAttributeType, nonControllerAttributeType,
                 controllerAttributeType, nonActionAttributeType, filterCollectionType);
@@ -65,7 +78,7 @@ namespace RockLib.Logging.AspNetCore.Analyzers
             context.RegisterSymbolAction(analyzer.AnalyzeMethod, SymbolKind.Method);
         }
 
-        private class Analyzer
+        private sealed class Analyzer
         {
             private readonly INamedTypeSymbol _infoLogAttributeType;
             private readonly INamedTypeSymbol _nonControllerAttributeType;
@@ -90,15 +103,14 @@ namespace RockLib.Logging.AspNetCore.Analyzers
 
                 if (IsController(namedTypeSymbol))
                 {
-                    if (HasInfoLogAttribute(namedTypeSymbol))
-                        return;
+                    if (HasInfoLogAttribute(namedTypeSymbol)) { return; }
 
                     foreach (var method in namedTypeSymbol.GetMembers().OfType<IMethodSymbol>())
-                        if (method.MethodKind == MethodKind.Ordinary && HasInfoLogAttribute(method))
-                            return;
+                    {
+                        if (method.MethodKind == MethodKind.Ordinary && HasInfoLogAttribute(method)) { return; }
+                    }
 
-                    if (IsInfoLogAttributeAddedToFilterCollection(context))
-                        return;
+                    if (IsInfoLogAttributeAddedToFilterCollection(context)) { return; }
 
                     var diagnostic = Diagnostic.Create(Rule, namedTypeSymbol.Locations[0], $"{namedTypeSymbol.Name} controller");
                     context.ReportDiagnostic(diagnostic);
@@ -115,10 +127,14 @@ namespace RockLib.Logging.AspNetCore.Analyzers
                 {
                     if (methodSymbol.MethodKind == MethodKind.Ordinary
                         && (HasInfoLogAttribute(containingType) || HasInfoLogAttribute(methodSymbol)))
+                    {
                         return;
+                    }
 
                     if (IsInfoLogAttributeAddedToFilterCollection(context))
+                    {
                         return;
+                    }
 
                     var diagnostic = Diagnostic.Create(Rule, methodSymbol.Locations[0], $"{methodSymbol.Name} action method");
                     context.ReportDiagnostic(diagnostic);
@@ -133,14 +149,15 @@ namespace RockLib.Logging.AspNetCore.Analyzers
 
             private bool IsInfoLogAttributeAddedToFilterCollection(SymbolAnalysisContext context)
             {
-                var visitor = new SyntaxWalker(_filterCollectionType, _infoLogAttributeType, context.CancellationToken);
-                visitor.Visit(context.Compilation);
+                var visitor = new SyntaxWalker(_filterCollectionType, _infoLogAttributeType,
+                    context.Compilation, context.CancellationToken);
                 return visitor.IsInfoLogAttributeAddedToFilterCollection;
             }
 
             // https://github.com/dotnet/aspnetcore/blob/62b77d0bad02e72b6675545392ecb3232d508e43/src/Mvc/Mvc.Core/src/Controllers/ControllerFeatureProvider.cs#L41
             private bool IsController(INamedTypeSymbol namedTypeSymbol)
             {
+                // TODO: Can probably simplify this.
                 if (namedTypeSymbol.IsValueType)
                     return false;
 
@@ -156,16 +173,17 @@ namespace RockLib.Logging.AspNetCore.Analyzers
                 if (IsAttributeDefined(namedTypeSymbol, _nonControllerAttributeType))
                     return false;
 
-                if (!namedTypeSymbol.Name.EndsWith("Controller")
+                if (!namedTypeSymbol.Name.EndsWith("Controller", StringComparison.InvariantCulture)
                     && !IsAttributeDefined(namedTypeSymbol, _controllerAttributeType))
                     return false;
-                
+
                 return true;
             }
 
             // https://github.com/dotnet/aspnetcore/blob/62b77d0bad02e72b6675545392ecb3232d508e43/src/Mvc/Mvc.Core/src/ApplicationModels/DefaultApplicationModelProvider.cs#L396
             private bool IsAction(IMethodSymbol methodSymbol)
             {
+                // TODO: Can probably simplify this.
                 if (methodSymbol.IsStatic)
                     return false;
 
@@ -193,29 +211,28 @@ namespace RockLib.Logging.AspNetCore.Analyzers
                 return true;
             }
 
-            private class SyntaxWalker : CSharpSyntaxWalker
+            private sealed class SyntaxWalker : CSharpSyntaxWalker
             {
                 private readonly INamedTypeSymbol _filterCollectionType;
                 private readonly INamedTypeSymbol _infoLogAttributeType;
                 private readonly CancellationToken _cancellationToken;
-                private Compilation _compilation;
+                private readonly Compilation _compilation;
 
                 public SyntaxWalker(INamedTypeSymbol filterCollectionType,
-                    INamedTypeSymbol infoLogAttributeType, CancellationToken cancellationToken)
+                    INamedTypeSymbol infoLogAttributeType, Compilation compilation, CancellationToken cancellationToken)
                 {
                     _filterCollectionType = filterCollectionType;
                     _infoLogAttributeType = infoLogAttributeType;
                     _cancellationToken = cancellationToken;
+                    _compilation = compilation;
+
+                    foreach (var syntaxTree in compilation.SyntaxTrees)
+                    {
+                        Visit(syntaxTree.GetRoot(_cancellationToken));
+                    }
                 }
 
                 public bool IsInfoLogAttributeAddedToFilterCollection { get; private set; }
-
-                public void Visit(Compilation compilation)
-                {
-                    _compilation = compilation;
-                    foreach (var syntaxTree in compilation.SyntaxTrees)
-                        Visit(syntaxTree.GetRoot(_cancellationToken));
-                }
 
                 public override void VisitInvocationExpression(InvocationExpressionSyntax node)
                 {
@@ -225,7 +242,7 @@ namespace RockLib.Logging.AspNetCore.Analyzers
                         && memberAccess2.Name.Identifier.Text == "Filters"
                         && _compilation.GetSemanticModel(node.SyntaxTree) is SemanticModel semanticModel
                         && semanticModel.GetOperation(node, _cancellationToken) is IInvocationOperation invocation
-                        && SymbolEqualityComparer.Default.Equals(invocation.Instance.Type, _filterCollectionType))
+                        && SymbolEqualityComparer.Default.Equals(invocation.Instance!.Type, _filterCollectionType))
                     {
                         if (invocation.TargetMethod.IsGenericMethod)
                         {
---
+++ Logging.AspNetCore/RockLib.Logging.AspNetCore.Analyzers/AddInfoLogAttributeCodeFixProvider.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using RockLib.Analyzers.Common;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RockLib.Logging.AspNetCore.Analyzers
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(AddInfoLogAttributeCodeFixProvider)), Shared]
+    public sealed class AddInfoLogAttributeCodeFixProvider : CodeFixProvider
+    {
+        public const string AddInfoLogAttributeTitle = "Add InfoLog attribute";
+
+        public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(DiagnosticIds.AddInfoLogAttribute);
+
+        public override sealed FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
+        public async override Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = (await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false))!;
+
+            foreach (var diagnostic in context.Diagnostics)
+            {
+                var node = (MemberDeclarationSyntax)root.FindNode(diagnostic.Location.SourceSpan);
+
+                context.RegisterCodeFix(
+                    CodeAction.Create(
+                        title: AddInfoLogAttributeTitle,
+                        createChangedDocument: cancellationToken =>
+                            AddInfoLogAttributeAsync(context.Document, root, node),
+                        equivalenceKey: nameof(AddInfoLogAttributeTitle)),
+                    diagnostic);
+            }
+        }
+
+        private static Task<Document> AddInfoLogAttributeAsync(Document document, SyntaxNode root, MemberDeclarationSyntax declaration)
+        {
+            var attributes = declaration.AttributeLists.Add(
+                SyntaxFactory.AttributeList(SyntaxFactory.SingletonSeparatedList<AttributeSyntax>(
+                    SyntaxFactory.Attribute(SyntaxFactory.IdentifierName("InfoLog")))));
+
+            root = root.ReplaceNode(declaration, declaration.WithAttributeLists(attributes));
+
+            if (root is CompilationUnitSyntax compilationUnit
+                && !compilationUnit.Usings.Any(u => u.Name.ToFullString() == "RockLib.Logging.AspNetCore"))
+            {
+                var name = SyntaxFactory.QualifiedName(
+                    SyntaxFactory.QualifiedName(
+                        SyntaxFactory.IdentifierName("RockLib"),
+                        SyntaxFactory.IdentifierName("Logging")),
+                    SyntaxFactory.IdentifierName("AspNetCore"));
+                root = compilationUnit.AddUsings(SyntaxFactory.UsingDirective(name));
+            }
+
+            return Task.FromResult(document.WithSyntaxRoot(root));
+        }
+    }
+}
